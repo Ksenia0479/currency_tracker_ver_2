a01b63a804688f0f29f3bb071a9f6c680cc5d547
Ksenia0479 <40492196+Ksenia0479@users.noreply.github.com>
2018-11-30 10:48:36 +0300

    Delete script.js

--- desktop/geotrack-akveo/script.js
+++
@@ -1,8 +0,0 @@
-function loadGoogleMap() {
-    let mapOptions = {
-        center: new google.maps.LatLng(53.55, 27.33),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
